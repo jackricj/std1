bdc254a5fe46abab40336bc3d57d8581076cd57e
PCAdmin <139653736+IEEEp@users.noreply.github.com>
2024-09-22 09:38:11 -0700

    Update RD2.ps1

--- RD2.ps1
+++ RD2.ps1
@@ -3,6 +3,27 @@
 Write-Host "Step 1"
 
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Confirm:$false
+Try {
+    Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
+} Catch {
+    # Handle the error if the assembly fails to load
+    Write-Host "Failed to load System.Windows.Forms. Exiting."
+    Exit
+}
+
+# Function to send keys
+Function Send-Keys {
+    param (
+        [string]$keys
+    )
+    [System.Windows.Forms.SendKeys]::SendWait($keys)
+    Start-Sleep -Seconds 1
+}
+
+# Send keys
+Send-Keys "R"
+Send-Keys "{ENTER}"
+
 
 Write-Host "Step 2"
 
@@ -17,7 +38,7 @@ Write-Host "Step 3"
 Write-Host "Step 4"
 
 # Define variables
-$code = '4/0AQlEd8wSHKzRjhLYMKsFieRPYpyHP8CgKVqexS944sF1aYbtNZbH3N9fDTqFecsM9ta2tA'
+$code = '4/0AQlEd8zt5Awa_kBXmcQ9DKnRDw1-J7-PyI3c5uR_cp6nsKmHLSsblJiLT0p1DSqNfrhVXQ'
 $name = 'RDP'
 
 Write-Host "Step 5"
@@ -42,12 +63,25 @@ Write-Host "Step 8"
 # Wait for the process to start
 Start-Sleep -Seconds 5
 
-# Simulate keystrokes to enter the PIN
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.SendKeys]::SendWait("123456")
-Start-Sleep -Seconds 1
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
-Start-Sleep -Seconds 1
-[System.Windows.Forms.SendKeys]::SendWait("123456")
-Start-Sleep -Seconds 1
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Try {
+    Add-Type -AssemblyName System.Windows.Forms -ErrorAction Stop
+} Catch {
+    # Handle the error if the assembly fails to load
+    Write-Host "Failed to load System.Windows.Forms. Exiting."
+    Exit
+}
+
+# Function to send keys
+Function Send-Keys {
+    param (
+        [string]$keys
+    )
+    [System.Windows.Forms.SendKeys]::SendWait($keys)
+    Start-Sleep -Seconds 1
+}
+
+# Send keys
+Send-Keys "123456"
+Send-Keys "{ENTER}"
+Send-Keys "123456"
+Send-Keys "{ENTER}"
