1516dcfeae4987aec21fdd8ebad94a3ccadea9af
PCAdmin <139653736+IEEEp@users.noreply.github.com>
2024-11-08 17:31:40 -0800

    d

---
+++ SOU_File.ps1
@@ -0,0 +1,44 @@
+
+
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Confirm:$false
+
+
+$username = $env:USERNAME
+
+# Start-Process powershell -Verb RunAs
+
+
+# Unblock-File -Path "C:\Users\$username\Downloads\RD2.ps1"
+
+
+# Define variables
+$code = '4/0AVG7fiRqIoNFE1FhtteGK4GMlXPaFXSuC63ooJ33MjP-zeFRTgC496Edci1oTfYbBOf8bQ'
+$name = 'ELzPC'
+
+
+# Download the Chrome Remote Desktop Host MSI
+Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command Invoke-WebRequest https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi -OutFile chromeremotedesktophost.msi" -WindowStyle Hidden -Wait
+
+
+
+Start-Process msiexec.exe -ArgumentList '/i chromeremotedesktophost.msi /quiet /norestart' -Wait
+
+
+Start-Sleep -Seconds 5
+
+# Start the Chrome Remote Desktop Host with the provided code and name
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit -Command & {& '${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe' --code='$code' --redirect-url='https://remotedesktop.google.com/_/oauthredirect' --name='$name' --pin='123456'}"
+
+
+# Wait for the process to start
+Start-Sleep -Seconds 5
+
+# Simulate keystrokes to enter the PIN
+#Add-Type -AssemblyName System.Windows.Forms
+#[System.Windows.Forms.SendKeys]::SendWait("123456")
+#Start-Sleep -Seconds 1
+#[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+#Start-Sleep -Seconds 1
+#[System.Windows.Forms.SendKeys]::SendWait("123456")
+#Start-Sleep -Seconds 1
+#[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
